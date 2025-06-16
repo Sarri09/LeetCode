631be4bdbb93715daa80f48c2aad5f18b60c77cb
Andy <72534502+Sarri09@users.noreply.github.com>
2025-06-15 22:15:52 -0400

    Time: 1 ms (34.71%) | Memory: 11.1 MB (22.00%) - LeetSync

---
+++ 67-add-binary/add-binary.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    string addBinary(string a, string b) {
+        string result = "";
+        int LargA = a.length() - 1;
+        int LargB = b.length() - 1;
+        int carry = 0;
+
+        while (LargA >= 0 || LargB >= 0 || carry) {
+            int sum = carry;
+            if (LargA >= 0) {
+                sum = sum + a[LargA] - '0';
+                LargA--;
+            }
+            if (LargB >= 0) {
+                sum = sum + b[LargB] - '0';
+                LargB--;
+            }
+            
+            result = char(sum % 2 + '0') + result;
+            carry = sum / 2;
+        }
+
+        return result;
+    }
+};
